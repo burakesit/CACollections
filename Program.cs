1587575c2914f6cb0e3d0f879052f901ee92f8fc
burak eşit <esitburak@hotmail.com>
2021-03-18 23:24:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CACollections;
+using System;
+using System.Collections.Generic;
+
+namespace OOP1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Ali" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
